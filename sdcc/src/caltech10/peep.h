9a48187ecdf6fa1e462e6bc95efae8cb29cb3b3c
unknown <sophialiu16@gmail.com>
2019-06-08 22:03:07 -0700

    added default peep probably does not work

--- sdcc/src/caltech10/peep.h
+++ sdcc/src/caltech10/peep.h
@@ -1,1 +1,14 @@
-//
+#ifndef PEEPC10_INCLUDE
+#define PEEPC10_INCLUDE
+//more files to inc?
+
+int getSize (lineNode * line);
+bitVect getRegsRead (lineNode * line);
+bitVect getRegsWritten (lineNode * line);
+bool deadMove (const char *reg, lineNode * currpl, lineNode * head);
+bool notUsed (const char *reg, lineNode * currPl, lineNode * head);
+bool canAssign (const char *op1, const char *op2, const char *op3);
+bool notUsedFrom (const char *reg, const char *label, lineNode *head);
+bool symmParmStack (void);
+
+#endif
